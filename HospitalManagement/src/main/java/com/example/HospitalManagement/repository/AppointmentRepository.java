24ee1dfdc5c6a29838881844fd75417ca6eb9d7c
iRahmanG <rahmanmr039@gmail.com>
2026-01-08 15:11:38 +0530

    Added repositories and services

---
+++ HospitalManagement/src/main/java/com/example/HospitalManagement/repository/AppointmentRepository.java
@@ -0,0 +1,4 @@
+package com.example.HospitalManagement.repository;
+
+public interface AppointmentRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.HospitalManagement.entity.Appointment, java.lang.Long> {
+  }
---
+++ HospitalManagement/src/main/java/com/example/HospitalManagement/repository/DepartmentRepository.java
@@ -0,0 +1,4 @@
+package com.example.HospitalManagement.repository;
+
+public interface DepartmentRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.HospitalManagement.entity.Department, java.lang.Long> {
+  }
